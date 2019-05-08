edec20bc0494616af42309087cacab09cb82500c
qinmaye <qinchi@nuaa.edu.cn>
2019-05-08 11:04:48 +0800

    完成IM,DM,Extend

--- lab1/lab1.srcs/sources_1/new/IM.v
+++ lab1/lab1.srcs/sources_1/new/IM.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2019/05/08 10:21:06
+// Create Date: 2019/05/08 10:33:49
 // Design Name: 
-// Module Name: Extend
+// Module Name: IM
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -18,12 +18,14 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
- //signal 是无符号拓展和有符号拓展标志，0为无符号拓展
-module Extend(
-    input   instr[15:0],
-    input   signal,
-    output dout[31:0]
-    );
+
+
+module im_4k(addr,dout);
+    input   [11:2]addr;
+    output  [31:0]dout;
+
+    reg     [31:0]im[1023:0];
+
+    assign dout = im[addr];
     
-    assign dout = (signal==1'b0) ? {(16'b0),instr[15:0]} : {{16{instr[15]}},instr[15:0]};
 endmodule
--- lab1/lab1.srcs/sources_1/new/dm.v
+++ lab1/lab1.srcs/sources_1/new/dm.v
@@ -24,8 +24,10 @@ module dm_4k(addr,din,we,clk,dout);
     input   [11:2]addr;
     input   [31:0]din;
     input   we;
-    input   clk ;
+    input   clk;
+
     output  [31:0]dout;
+
     reg     [31:0]dm[1023:0];
 
     assign dout = dm[addr];
@@ -34,5 +36,5 @@ module dm_4k(addr,din,we,clk,dout);
             if(we)
                 dm[addr] <= din;
         end
-    
+
 endmodule
