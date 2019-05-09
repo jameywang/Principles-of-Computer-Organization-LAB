690b9ae70705b3c6696d2adc25234d6abaee6a8c
qinmaye <qinchi@nuaa.edu.cn>
2019-05-09 18:34:01 +0800

    完成ALU control

--- lab1/lab1.srcs/sources_1/new/ALU_Control.v
+++ lab1/lab1.srcs/sources_1/new/ALU_Control.v
@@ -21,6 +21,20 @@
 
 
 module ALU_Control(
-
+    input   [2:0]ALUop,
+    input   [5:0]Instr,
+    output  [2:0]ALUctr
     );
+
+    always@(ALUop or Instr)
+    begin
+        if(ALUop == 3'b001)
+            ALUctr = 3'b001;
+        else if(ALUop == 3'b010)
+            ALUctr = 3'b010;
+        else if(ALUop == 3'b011)
+            ALUctr = 3'b011;
+        else if(ALUop == 3'b111)
+            ALUctr = 3'b111; 
+    end
 endmodule
