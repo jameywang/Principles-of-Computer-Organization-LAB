40e2931d90fe44811b2d666a5418758e73b2da4a
qinchi@nuaa.edu.cn <qinchi@nuaa.edu.cn>
2019-05-11 16:26:19 +0800

    完成mips

--- lab1/lab1.srcs/sources_1/new/MIPS.v
+++ lab1/lab1.srcs/sources_1/new/MIPS.v
@@ -21,6 +21,21 @@
 
 
 module MIPS(
-
+    input   clk,
+    input   reset
     );
+
+    wire    branch;
+    wire    jump;
+    wire    memtoreg;
+    wire    memwrite;
+    wire    alusrc;
+    wire    regwrite;
+    wire    regdst;
+    wire    [2:0]aluctr;
+    wire    [31:0]data;
+
+    datapath datap(clk,reset,aluctr,jump,branch,memtoreg,memwrite,alusrc,regwrite,regdst,data);
+    control control1(data[31:26],data[4:0],jump,branch,memtoreg,memwrite,alusrc,regwrite,regdst,aluctr);
+
 endmodule
