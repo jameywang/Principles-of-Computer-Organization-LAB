1ba7ba2d400973739cfad90bd8d07c039747e0a4
qinchi@nuaa.edu.cn <qinchi@nuaa.edu.cn>
2019-05-11 17:11:47 +0800

    修改ALU

--- lab1/lab1.srcs/sources_1/new/ALU.v
+++ lab1/lab1.srcs/sources_1/new/ALU.v
@@ -24,24 +24,24 @@ module ALU(
     input   [31:0]data1,
     input   [31:0]data2,
     input   [2:0]ALUctr,
-    output  [31:0]result,
+    output  reg [31:0]result,
     output  zero
     );
 
     always@(data1 or data2 or ALUctr)
     begin
         if(ALUctr==3'b001)
-            result = data1 + data2;
+            result <= data1 + data2;
         else if(ALUctr==3'b010)
-            result = data1 & data2;
+            result <= data1 & data2;
         else if(ALUctr==3'b101)
-            result = data1 - data2;
+            result <= data1 - data2;
         else if(ALUctr==3'b011)
-            result = data1 | data2;
+            result <= data1 | data2;
         else if(ALUctr==3'b111)
-            result = (data1 < data2) ? 32'b1 : 32'b0;
+            result <= (data1 < data2) ? 32'b1 : 32'b0;
         
-        zero = (result == 32'b0) ? 1'b1 : 1'b0;
+        zero <= (result == 32'b0) ? 1'b1 : 1'b0;
 
     end
 endmodule
