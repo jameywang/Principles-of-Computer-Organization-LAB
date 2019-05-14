1f683726ffdcbe78e573517c49f9bfad74427dc6
qinchi@nuaa.edu.cn <qinchi@nuaa.edu.cn>
2019-05-14 17:20:01 +0800

    补充助教没说的指令

--- lab1/lab1.srcs/sources_1/new/IM.v
+++ lab1/lab1.srcs/sources_1/new/IM.v
@@ -27,7 +27,7 @@ module im_4k(addr,dout);
     reg     [31:0]im[1023:0];
 
     initial
-        $readmemh("D:/verilog/lab/lab1/code.txt",im);
+        $readmemh("C:/files/PA-LAB/lab1/code.txt",im);
     assign dout = im[addr];
     
     
